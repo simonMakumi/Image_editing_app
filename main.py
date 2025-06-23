c72735297c86a33a2a26048bdb2fad0d2613da13
Simon Makumi <simonmakumi5@gmail.com>
2025-06-23 12:45:11 +0300

    App Layout

---
+++ main.py
@@ -0,0 +1,68 @@
+# Imports Modules
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QListWidget, QPushButton, QHBoxLayout, QVBoxLayout, QComboBox
+from PyQt5.QtCore import Qt
+
+# App Settings
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("PhotoQT")
+main_window.resize(900,700)
+
+# All app widgets
+btn_folder = QPushButton("Folder")
+file_list = QListWidget()
+
+btn_left = QPushButton("Left")
+btn_right = QPushButton("Right")
+mirror = QPushButton("Mirror")
+sharpness = QPushButton("Sharpen")
+gray = QPushButton("B/W")
+saturation = QPushButton("Color")
+contrast = QPushButton("Contrast")
+blur = QPushButton("Blur")
+
+# Dropdown box
+filter_box = QComboBox()
+filter_box.addItem("Original")
+filter_box.addItem("Left")
+filter_box.addItem("Right")
+filter_box.addItem("Mirror")
+filter_box.addItem("Sharpen")
+filter_box.addItem("B/W")
+filter_box.addItem("Color")
+filter_box.addItem("Contrast")
+filter_box.addItem("Blur")
+
+picture_box = QLabel("Image will appear here")
+
+
+# App Design
+master_layout = QHBoxLayout()
+
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+
+col1.addWidget(btn_folder)
+col1.addWidget(file_list)
+col1.addWidget(filter_box)
+col1.addWidget(btn_left)
+col1.addWidget(btn_right)
+col1.addWidget(mirror)
+col1.addWidget(sharpness)
+col1.addWidget(gray)
+col1.addWidget(saturation)
+col1.addWidget(contrast)
+col1.addWidget(blur)
+
+col2.addWidget(picture_box)
+
+master_layout.addLayout(col1, 20)
+master_layout.addLayout(col2, 80)
+
+main_window.setLayout(master_layout)
+
+
+
+# Show/Run
+main_window.show()
+app.exec_()
